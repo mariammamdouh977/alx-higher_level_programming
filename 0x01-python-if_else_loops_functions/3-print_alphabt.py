1234094c718c0f6081b77bba538e3ee9c63eb101
mariammamdouh977 <mariammamdouh977@gmail.com>
2023-10-04 11:21:49 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letters in range(97,123):
     if chr(letters) != "q" or chr(letters) != "e":
-        print(f"{chr(letters)}", end="")
+        print("{}".format(chr(letters), end="")
