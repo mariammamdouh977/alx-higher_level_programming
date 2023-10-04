e00ba377c4e0b341db2d13c65ffe5bc2c08cd178
mariammamdouh977 <mariammamdouh977@gmail.com>
2023-10-04 13:40:41 -0700

    14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
+for i in range(25, -1, -1):
+    c = i + ord('A')
+    if i % 2 == 1:
+        c += 32
+    print("{:c}".format(c), end="")    
