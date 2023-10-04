22ddf44ceddf4f4bdbb16b9bc5ae1cac419032fb
mariammamdouh977 <mariammamdouh977@gmail.com>
2023-10-04 11:23:47 -0700

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letters in range(97,123):
-    if chr(letters) != "q" or chr(letters) != "e":
-        print("{}".format(chr(letters), end="")
+# Author - Godswill Kalu
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
